286c264227d7875c4a627cf61c3cc11593a1f878
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-24 20:55:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,17 +5,17 @@ def reverse_each_word_with_each(string)
   #.reverse reverses an array
   original_array = string.split(" ")
   return_array = []
-  original_array.each do|string|
+  original_array.each {|string|
     return_array << string.reverse
-  end
+  }
   return_array.join(" ")
 end
 
 def reverse_each_word(string)
   array = string.split(" ") #turn string into an array
   test_array = []
-  array.collect do|string| #iterate over the array
+  array.collect {|string| #iterate over the array
     test_array << string.reverse #reverse each word in the array
-  end
+  }
   test_array.join(" ")
 end
